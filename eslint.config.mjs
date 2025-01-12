fd206f83dce2dafbd3ef1569f0a414a980b97b98
nicofishman <74215078+nicofishman@users.noreply.github.com.>
2025-01-12 19:48:44 -0300

    add: initial configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import unusedImportPlugin from 'eslint-plugin-unused-imports';
+import globals from 'globals';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +14,38 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    ignores: ['node_modules', '.next', 'out', 'public'],
+  },
+  {
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+      globals: {
+        ...globals.jest,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'unused-imports': unusedImportPlugin,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+      ],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', args: 'none' }],
+      'unused-imports/no-unused-imports': 'warn',
+      'react/function-component-definition': [
+        'warn',
+        {
+          namedComponents: 'function-declaration',
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
